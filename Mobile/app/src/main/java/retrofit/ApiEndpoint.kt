24eb3d4a7423c7805865d35d9e2fc99df6774451
Fallure <mfatur757@gmail.com>
2023-02-12 15:27:29 +0700

    Memperbaiki input dari NIM

--- Mobile/app/src/main/java/retrofit/ApiEndpoint.kt
+++ Mobile/app/src/main/java/retrofit/ApiEndpoint.kt
@@ -1,8 +1,10 @@
 package retrofit
 
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ApiEndpoint {
-    @GET("posts")
-    fun getPosts(): Call<ArrayList<PostResponse>>
+//    @GET("posts")
+//    fun getPosts(): Call<ArrayList<PostResponse>>
+//    @POST
 }
