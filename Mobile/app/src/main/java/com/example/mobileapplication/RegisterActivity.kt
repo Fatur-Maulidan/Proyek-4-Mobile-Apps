9411b3e111120e4b36622263aa32363c4dfd573d
Aldrin Rayhan Putra <aldrin.rayhan.tif21@polban.ac.id>
2023-02-13 08:28:34 +0700

    Update RegisterActivity.kt
    Mengubah handling untuk field nim, email, konfirmasi password, dan nama (terdapat bug)

--- Mobile/app/src/main/java/com/example/mobileapplication/RegisterActivity.kt
+++ Mobile/app/src/main/java/com/example/mobileapplication/RegisterActivity.kt
@@ -36,11 +36,6 @@ class RegisterActivity : AppCompatActivity() {
         val varImgViewShowPass1: ImageView = findViewById(R.id.imageViewShowPassword1)
         val varImgViewShowPass2: ImageView = findViewById(R.id.imageViewShowPassword2)
 
-        fun isEtEmpty(editTextVar: EditText): Boolean {
-            if (editTextVar.text.toString() == "") return true
-            return false
-        }
-
         fun passwordToggle(editText: EditText, imageView: ImageView) {
             imageView.setOnClickListener {
                 if (editText.transformationMethod == null) {
@@ -57,19 +52,64 @@ class RegisterActivity : AppCompatActivity() {
         passwordToggle(varEtKonfirmasiKataSandi, varImgViewShowPass2)
 
         varBtnDaftar.setOnClickListener(View.OnClickListener {
+            // Handle untuk nama
             varTvNama.text =
                 if (varEtNama.text.toString().isEmpty()) "NAMA TIDAK BOLEH KOSONG" else null
-            varTvNIM.text =
-                if (varEtNim.text.toString().isEmpty()) "NIM TIDAK BOLEH KOSONG" else null
-            varTvEmail.text =
-                if (varEtEmail.text.toString().isEmpty()) "E-MAIL TIDAK BOLEH KOSONG" else null
+
+            if (varEtNama.text.toString().isEmpty()) {
+                varTvNama.text = "NAMA TIDAK BOLEH KOSONG"
+            } else {
+                // Experimental
+                if (varEtNama.text.contains("^[ A-Za-z]+\$")) {
+                    varTvNama.text = "NAMA HANYA BOLEH MENGGUNAKAN ALFABET DAN SPASI"
+                } else {
+                    varTvNama.text = null
+                }
+            }
+
+            // Handle untuk NIM
+            if (varEtNim.text.toString().isEmpty()) {
+                varTvNIM.text = "NIM TIDAK BOLEH KOSONG"
+            } else {
+                if (varEtNim.text.length < 9) {
+                    varTvNIM.text = "NIM HARUS BERJUMLAH 9 DIGIT"
+                } else {
+                    varTvNIM.text = null
+                }
+            }
+
+            // Handle untuk e-mail
+            if (varEtEmail.text.toString().isEmpty()) {
+                varTvEmail.text = "E-MAIL TIDAK BOLEH KOSONG"
+            } else {
+                var isEmailValid: (String, String) -> Boolean =
+                    { email: String, emailParameter: String -> email.contains(emailParameter) }
+                if (!isEmailValid(varEtEmail.text.toString(), "polban.ac.id")) {
+                    varTvEmail.text = "WAJIB MENGGUNAKAN E-MAIL POLBAN"
+                } else {
+                    varTvEmail.text = null
+                }
+            }
+
+            // Handle untuk kata sandi
             varTvKataSandi.text =
                 if (varEtKataSandi.text.toString()
                         .isEmpty()
                 ) "KATA SANDI TIDAK BOLEH KOSONG" else null
-            varTvKonfirmasiKataSandi.text = if (varEtKonfirmasiKataSandi.text.toString()
-                    .isEmpty()
-            ) "MASUKAN KATA SANDI YANG SAMA" else null
+
+            // Handle untuk konfirmasi kata sandi
+            when {
+                varEtKonfirmasiKataSandi.text.toString()
+                    .isEmpty() -> {
+                    varTvKonfirmasiKataSandi.text = "MASUKAN KONFIRMASI KATA SANDI"
+                }
+                varEtKataSandi.text.toString() != varEtKonfirmasiKataSandi.text.toString() -> {
+                    varTvKonfirmasiKataSandi.text = "KATA SANDI TIDAK COCOK"
+                }
+                else -> {
+                    varTvKonfirmasiKataSandi.text = null
+                }
+            }
         })
     }
 }
